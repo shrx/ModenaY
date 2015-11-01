fdef7a6c5d3ea8f5d59c7521d760f8cab5edc648
shrx <differenxe@gmail.com>
2015-11-01 14:15:30 +0100

    First commit

---
+++ ModenaY/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModenaY
+//
+//  Created by Martin G. on 1. 11. 15.
+//  Copyright © 2015 Martin G. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ModenaY/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ModenaY
+//
+//  Created by Martin G. on 1. 11. 15.
+//  Copyright © 2015 Martin G. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        println("test")
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
